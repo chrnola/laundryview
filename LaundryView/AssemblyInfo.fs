25764aa67f2dea8fd365c7590a92d26d9332f68c
Chris Pinola <chris@pinola.co>
2017-10-29 18:21:46 -0400

    Add paket.template and cleanup references

--- LaundryView/AssemblyInfo.fs
+++ LaundryView/AssemblyInfo.fs
@@ -7,12 +7,12 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("laundryview")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyTitle("LaundryView")>]
+[<assembly: AssemblyDescription("A library for interacting with the proprietary LaundryView APIs and parsing their results.")>]
 [<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("laundryview")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyCompany("Chris Pinola")>]
+[<assembly: AssemblyProduct("LaundryView")>]
+[<assembly: AssemblyCopyright("")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.1.0")>]
 
 do
     ()
